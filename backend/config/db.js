94a55217d610e2a9e6c1f143810c25f1ae6af74f
chimichi004 <chember004@gmail.com>
2023-02-02 17:12:47 +0800

    REST API Goals

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+
+        await mongoose.set("strictQuery", false);
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan)
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
