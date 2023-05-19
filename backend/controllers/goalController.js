6a6874335dd44c92b8abe39151fbf96d42628f9f
chimichi004 <chember004@gmail.com>
2023-05-19 15:05:51 +0800

    Add user model/schema, controller,model, and routes. Add authentication, jwt, login

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,12 @@
 const asyncHandler = require('express-async-handler');
-
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 // @access Private
 // @desc Get goals
 // @route GET /api/goals
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({ user: req.user.id});
     res.status(200).json(goals);
 });
 
@@ -21,7 +21,8 @@ const setGoal = asyncHandler(async (req, res) => {
     }
 
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.idq
     })
     res.status(200).json(goal);
 })
@@ -36,6 +37,20 @@ const updateGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+    //Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('USer not found')
+    }
+
+    //Make sure the logged in user matches to goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true,});
 
@@ -52,6 +67,19 @@ const deleteGoal= asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+    //Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('USer not found')
+    }
+
+    //Make sure the logged in user matches to goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    } 
 
     await goal.remove();
 
--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`server start on port ${port}`));
+app.listen(port, () => console.log(`server start on port ${port}`))
